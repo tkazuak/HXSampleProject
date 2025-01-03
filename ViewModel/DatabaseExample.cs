e60324b17bfe8e10d4e53658a62c611b369e7348
tkazuak <tkazuak@gmail.com>
2025-01-04 00:11:32 +0900

    SQL Serverへの接続の確認が完了できた。そこまでのソース

--- ViewModel/DatabaseExample.cs
+++ ViewModel/DatabaseExample.cs
@@ -1,18 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System;
-using System.Data.SqlClient;
+using System.Windows;  // MessageBox を使うために必要
 
 namespace HX_Sample_App.ViewModel
 {
     public class DatabaseExample
     {
+        // データベースに接続するメソッド
         public void ConnectToDatabase()
         {
+            // app.config に記載した接続文字列を取得
+            // ここでは "MyDbConnection" という名前で接続文字列を取得しています
             string connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["MyDbConnection"].ToString();
 
             // SQL Server への接続
@@ -20,27 +18,35 @@ namespace HX_Sample_App.ViewModel
             {
                 try
                 {
+                    // データベースへの接続を開く
                     connection.Open();
 
-                    // SQL クエリを作成
-                    string query = "SELECT TOP 10 * FROM dbo.UserInfo";
+                    // 接続成功のメッセージボックスを表示
+                    MessageBox.Show("データベースに接続しました。", "接続成功", MessageBoxButton.OK, MessageBoxImage.Information);
 
-                    // SQL コマンドを実行
-                    SqlCommand command = new SqlCommand(query, connection);
-                    SqlDataReader reader = command.ExecuteReader();
+                    // 実行する SQL クエリを定義
+                    string query = "SELECT TOP 10 user_id, user_password FROM dbo.UserInfo";  // dbo.UserInfo テーブルからデータを取得
 
-                    // データを読み取る
-                    while (reader.Read())
+                    // SQL コマンドを実行する準備
+                    using (SqlCommand command = new SqlCommand(query, connection))
                     {
-                        Console.WriteLine("User ID: " + reader["user_id"]);
-                        Console.WriteLine("User Password: " + reader["user_password"]);
+                        // クエリを実行して、結果を SqlDataReader で読み込む
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            // データを1行ずつ読み取る
+                            while (reader.Read())
+                            {
+                                // 読み取ったデータをメッセージボックスに表示
+                                string message = $"User ID: {reader["user_id"]}, User Password: {reader["user_password"]}";
+                                MessageBox.Show(message, "ユーザー情報", MessageBoxButton.OK, MessageBoxImage.Information);
+                            }
+                        }
                     }
-
-                    reader.Close();
                 }
                 catch (Exception ex)
                 {
-                    Console.WriteLine("エラー: " + ex.Message);
+                    // 例外が発生した場合、そのエラーメッセージを表示
+                    MessageBox.Show($"エラーが発生しました: {ex.Message}", "エラー", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
             }
         }
--- ViewModel/LoginViewModel.cs
+++ ViewModel/LoginViewModel.cs
@@ -1,12 +1,46 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace HX_Sample_App.ViewModel
 {
-    internal class LoginViewModel
+    public class LoginViewModel
     {
+        /// <summary>
+        /// ログインボタンのコマンド
+        /// </summary>
+        public ICommand LogionCheck { get; private set; }
+
+        public LoginViewModel()
+        {
+            // コマンドの初期化（実行ロジックと実行可否ロジック）
+            // RelayCommandは、コマンドの実行メソッド(ExecuteLogin)と実行可否メソッド(CanExecuteLogin)を指定する
+            LogionCheck = new RelayCommand(
+                ExecuteLogin // ログイン処理を実行するメソッド
+                             // , CanExecuteLogin // 必要に応じて、実行可能かどうかを判定するメソッド
+            );
+        }
+
+        /// <summary>
+        /// ログインボタンクリック時の処理
+        /// </summary>
+        /// <param name="parameter">コマンドが呼び出されたときに渡されるパラメータ</param>
+        private void ExecuteLogin(object? parameter)
+        {
+            // ログイン処理が実行されたことをコンソールに出力
+            Console.WriteLine("ログイン処理を実行しました！");
+
+            // データベース接続チェック処理を呼び出す
+            // DatabaseExample クラスの ConnectToDatabase メソッドを実行して、DB 接続処理を行う
+            DatabaseExample dbcheck = new DatabaseExample();
+            dbcheck.ConnectToDatabase();
+        }
+
+        // コマンドの実行可否を判定するメソッド（オプション）
+        // ここでは、引数として渡される parameter が null でない場合にログイン可能としています
+        private bool CanExecuteLogin(object? parameter)
+        {
+            // 例えば、ログインの条件が入力されたパラメータが null でない場合にログイン可能とする
+            return parameter != null;
+        }
     }
-    }
+}
